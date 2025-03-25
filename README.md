d414142f6471a71872ed4786e702d8c8b0d49250
Dregodblack <nascimento.andreluis18@gmail.com>
2025-03-25 03:34:35 -0300

    Version 7 - functions created and structure defined

--- README.md
+++ README.md
@@ -2,140 +2,27 @@
 
 <h3>Objetivo: Criar um reprodutor de música inspirado no Spotify.</h3><br>
 
-<h4>Segue abaixo uma breve instrução de como funciona essa aplicação:</h4>
+<h4>- Breve instrução de como funciona essa aplicação:</h4>
 <p>Na pasta (developing) se encontra essses arquivos "HTML, CSS e JavaScript" e dentro de cada um existe um roteiro baseados em códigos que se conectam entre si fazendo com que o software que esta em constante desenvolvimento seja funcional e bem simples de se usar. 
-Usando como base de uma pagina web do próprio html, junto com alguns estilos formados pelo css, e as manipulações sendo feitas pelo javaScript, nisso temos as chamadas que fazem conexão com a outra pasta, sendo ela (artifacts) ela contem arquivos do tipo imagens e musicas, sendo necesários pra execução da aplicação.</p><br>
+Usando como base de uma pagina web do próprio html, junto com alguns estilos formados pelo css, e as manipulações sendo feitas pelo javaScript, nisso temos as chamadas que fazem conexão com a outra pasta, sendo ela (artifacts) que contem arquivos do tipo imagens e musicas, sendo necesários pra execução da aplicação.</p><br>
+
+
+<h4>- Segue abaixo um exemplo de como esta o visual do reprodutor de músicas:</h4>
+
+<!-- IMG_Execução -->
+<div align="center">
+    <img alt="imagem_Tema_spotify" src="/artifacts/images/theme/Tema_spotify1.png"></img>
+</div><br>
 
 <div align="center">
-    <img alt="imagem_execution_music" src="/artifacts/images/execution_music.png"></img>
+    <img alt="imagem_Tema_spotify" src="/artifacts/images/theme/Tema_spotify2.png"></img>
 </div><br>
+
+
+Como apresentação para o usuario, foi deixado como registro no reprodutor de música o nome do cantor e a banda
+Nele, é possível Ouvir músicas, pausar e dar play quando quiser... também tem a opção de trocar de música indo para frente ou para trás na playlist, outro ponto é que podemos deixar a música atual para se repetir quantas vezes quiser, e podemos também embaralhar a ordem da playlist ativando o modo aleatório, e por fim é possível curtir a música que acabou de ouvir ativando o coração de favoritos.
 
 ##
 
-<h3>Segue abaixo uma descrição de como funciona cada parte do código:</h3>
-
-<h4>- Estrutura HTML:<h4>
-<div>
-    
-    O HTML define a estrutura do reprodutor de música, que inclue:
-    
-    Definição do arquivo:
-        DOCTYPE: Tipo do documento, no caso HTML
-        lang: Linguagem atribuida a pagina web ("pt-BR" português) ("en" inglês)
-
-    Definição do Cabeçalho:
-        Meta charset: Define a codificação de caracteres.
-        Meta viewport: Configura a visualização para dispositivos móveis.
-        Title: O título da página.
-        Link 1: Inclui o Bootstrap Icons para CSS personalizado
-        Link 2: Chama um arquivo do tipo CSS para realizar as iterações com a pagina web
-        
-    Definição do Corpo:
-        Div id music-container: Contém toda a estrutura do reprodutor de música.
-        Playlist title: Título da playlist/ álbum.
-        Cover image: Imagem da capa do álbum/música.
-        Audio element: Elemento de áudio HTML para reprodução.
-
-    Div id "below-cover"
-        Song info: Contém o nome da música e da banda.
-        Like button: Botão de "favorito" com um ícone de coração.
-
-        Div id "progress-container"
-           Progress container: Barra de progresso da música.
-
-        Div id "progress-container"
-            Button container: Contém os botões de controle (shuffle, previous, play, next, repeat).
-
-        script: Chama um arquivo do tipo JavaScript para realizar as iterações com a pagina web
-</div> 
-    
-<h4>- Estrutura CSS:</h4>
-<div> 
- 
-    O CSS define a aparência e a organização do reprodutor de música, ou seja, ele centraliza o conteúdo, ajusta cores, tamanhos e espaçamentos, e a aparência das funcionalidades.
-
-    Características do Corpo da Página (body)
-
-        background-image: Define um gradiente linear como fundo da página.
-        height: Define a altura do corpo da página para ocupar toda a altura da janela de visualização.
-        color: Define a cor do texto como branca.
-        font-family: Define a fonte do texto como sans-serif.
-        display: Usa flexbox para layout responsivo.
-        flex-direction: Define a direção principal do layout como coluna.
-        align-items: Centraliza os itens verticalmente.
-
-        Características do Título da Playlist (playlist-title)
-
-            display: Usa flexbox para layout responsivo.
-            flex-direction: Define a direção principal do layout como linha.
-            justify-content: Centraliza oconteúdo horizontalmente.
-            margin: Define a margem superior e inferior para espaçamento.
-                
-        Características do Nome da Música (song-name)
-
-            font-size: Define o tamanho dafonte.
-            font-weight: Define a espessura da 
-            fonte como mais grossa (bolder).
-
-        Características da Imagem de Capa (cover)
-
-            width: Define a largura da imagem.
-            height: Define a altura da imagem.
-            
-        Características da Div abaixo da Capa (below-cover)
-
-            display: Usa flexbox para layout responsivo.
-            flex-direction: Define a direção principal do layout como linha.
-            justify-content: Distribui espaço entre os itens.
-            align-items: Centraliza os itens verticalmente.
-            margin: Define a margem superior e inferior para espaçamento.
-
-        Características de Cores Claras (light-color)
-
-            color: Define a cor do texto.
-
-        Características do Contêiner de Progresso (progress-container)
-
-            padding-bottom: Adiciona preenchimento na parte inferior para torná-lo clicável.
-
-        Características da Barra de Progresso (progress-bar)
-
-            background-color: Define a cor de fundo.
-            height: Define a altura da barra.
-            width: Define a largura da barra.
-            border-radius: Define bordas arredondadas.
-
-        Características do Progresso Atual (current-progress)
-
-            --progress: Define uma variável CSS para o progresso.
-            background-color: Define a cor de fundo.
-            height: Herda a altura do elemento pai.
-            width: Define a largura com base na variável de progresso.
-            border-radius: Herda as bordas arredondadas do elemento pai.
-            
-        Características do Contêiner de Botões (button-container)
-
-            display: Usa flexbox para layout responsivo.
-            flex-direction: Define a direção principal do layout como linha.
-            justify-content: Distribui espaço ao redor dos itens.
-            align-items: Centraliza os itens verticalmente.
-            
-        Características dos Botões (button)
-
-            background-color: Define a cor de fundo como herdada do elemento pai.
-            color: Define a cor do texto como herdada do elemento pai.
-            border: Remove a borda.
-            
-        Características Específica dos Botões (.button)
-
-            .button: Define o tamanho da fonte.
-            .button-navigate: Define um tamanho maior para botões de navegação.
-            .button-biggest: Define o maior tamanho de fonte para o botão principal.
-</div>
-
-
-<h4>- Estrutura JavaScript:</h4>
-<div>
-    
-        ...
-</div>
+Segue abaixo o Link da playlist do youtube no qual usei como base para criar esse projeto:
+<a href="https://www.youtube.com/playlist?list=PLpdAy0tYrnKwSUtzFssaLV-KaOWAccdql"><b>Playlist Hashtag</b></a>
--- developing/script.js
+++ developing/script.js
@@ -1,36 +1,53 @@
+// Variaveis com referencias dos elementos "ID" HTML/CSS:
 const song = document.getElementById('audio');                              //tag audio
 const cover = document.getElementById('cover');                             //tag img
 const songName = document.getElementById('song-name');                      //tag nome da musica
 const bandName = document.getElementById('band-name');                      //tag nome da banda
 const play = document.getElementById('play');                               //tag botão play
-const next = document.getElementById('next');                               //tag botão next
-const previous = document.getElementById('previous');                       //tag botão previous
+const next = document.getElementById('next');                               //tag botão Próximo
+const previous = document.getElementById('previous');                       //tag botão Anterior
+const shuffleButton = document.getElementById('shuffle');                   //tag botão embaralhar
+const repeatButton = document.getElementById('repeat');                     //tag botão repetir
+const likeButton = document.getElementById('like');                         //tag botão favoritar
 const currentProgress = document.getElementById('current-progress');        //style do css da bara de progresso
 const progressContainer = document.getElementById('progress-container');    //tag da bara de progresso
+const songTime = document.getElementById('song-time');                      //tag Tempo atual da musica
+const totalTime = document.getElementById('total-time');                    //tag Tempo total da musica
 
+
+// Musicas & suas caracteristicas:
 const music0 = {
     Name_music: "4am",
     artist: "Justin Stone",
-    file: "Justin_Stone_4am"
+    file: "Justin_Stone_4am",
+    liked: false
 };
 
 const music1 = {
     Name_music: `Mun Rá`,
     artist: "Sabotage",
-    file: "Sabotage_Mun_Ra"
+    file: "Sabotage_Mun_Ra",
+    liked: false
 };
 
 const music2 = {
     Name_music: "Runaway",
     artist: "Galantis",
-    file: "Galantis_Runaway"
+    file: "Galantis_Runaway",
+    liked: false
 };
 
+// Variaveis auxiliares:
 let isPlaying = false;
-const playlist = [music0, music1, music2];
+let isShuffled = false;
+let isRepeating = false;
+
+// Array de playlist das musicas com database do navegador:
+const originalPlaylist = JSON.parse(localStorage.getItem('Playlist')) ?? [music0, music1, music2];
+let sortedPlaylist = [...originalPlaylist];
 let index = 0;
 
-
+// Funções:
 function playsong() {
     play.querySelector('.bi').classList.remove('bi-play-circle-fill');
     play.querySelector('.bi').classList.add('bi-pause-circle-fill');
@@ -39,8 +56,8 @@ function playsong() {
 }
 
 function pausesong() {
-    play.querySelector('.bi').classList.add('bi-play-circle-fill');
     play.querySelector('.bi').classList.remove('bi-pause-circle-fill');
+    play.querySelector('.bi').classList.add('bi-play-circle-fill');
     song.pause();
     isPlaying = false;
 }
@@ -55,15 +72,16 @@ function playPauseDecider() {
 }
 
 function initializeSong() {
-    cover.src = `/artifacts/images/${playlist[index].file}.jpg`;
-    song.src = `/artifacts/songs/${playlist[index].file}.mp3`;
-    songName.innerText = playlist[index].Name_music;
-    bandName.innerText = playlist[index].artist;
+    cover.src = `/artifacts/images/cover/${sortedPlaylist[index].file}.jpg`;
+    song.src = `/artifacts/songs/${sortedPlaylist[index].file}.mp3`;
+    songName.innerText = sortedPlaylist[index].Name_music;
+    bandName.innerText = sortedPlaylist[index].artist;
+    likeButtonManipulate();
 }
 
 function previousSong(){
     if(index === 0){
-        index = playlist.length - 1;
+        index = sortedPlaylist.length - 1;
     }
     else{
         index -= 1;
@@ -73,7 +91,7 @@ function previousSong(){
 }
 
 function nextSong(){
-    if(index === playlist.length - 1){
+    if(index === sortedPlaylist.length - 1){
         index = 0;
     }
     else{
@@ -83,26 +101,115 @@ function nextSong(){
     playsong();
 }
 
-function updateProgressBar(){                                             //Aqui atualiza a barra de progresso da musica com o tempo de duração
+function updateProgress(){
     //currentTime = tempo atual da musica em segundos
     //duration = duração total da musica (3 min)
     const barwidth = (song.currentTime/song.duration)*100;                     
     currentProgress.style.setProperty('--progress', `${barwidth}%`);
+    songTime.innerText = timeProgress(song.currentTime);
 }
 
-function jumpTo(event){                                                   //Aqui atualiza a barra de progresso da musica após um click dentro do tempo de duração
-    //parametro event, usado para receber o click
-    const width = progressContainer.clientWidth;                          //clientWidth = largura total da barra de progresso
-    const clickPosition = event.offsetX;                                  //offsetX = defini aonde foi o click na largura da barra de progresso
-    const jumpToTime = (clickPosition/width)* song.duration;              //
-    song.currentTime = jumpToTime;                                        //
+function jumpTo(event){                                     
+    //parametro event''', usado para receber o click
+    const width = progressContainer.clientWidth;            
+    const clickPosition = event.offsetX;                    
+    const jumpToTime = (clickPosition/width)* song.duration;
+    song.currentTime = jumpToTime;                          
+}
+
+function embaralhamento(array) {
+    const tamanhoArray = array.length;
+    let indiceAtual = tamanhoArray - 1;
+
+    while (indiceAtual > 0) { 
+        let indiceAleatorio = Math.floor(Math.random() * (indiceAtual + 1));
+        [array[indiceAtual], array[indiceAleatorio]] = [array[indiceAleatorio], array[indiceAtual]];
+        indiceAtual -= 1;
+    }
+    return array; // Retorna o array embaralhado
+}
+
+function shuffleButtonClicked() {
+    if (!isShuffled) {
+        isShuffled = true;
+        sortedPlaylist = embaralhamento([...originalPlaylist]); // Embaralha uma cópia
+        shuffleButton.classList.add('button-active');
+    } else {
+        isShuffled = false;
+        sortedPlaylist = [...originalPlaylist]; // Volta à ordem original
+        shuffleButton.classList.remove('button-active');
+    }
+}
+
+function repeatButtonClicked() {
+    if (isRepeating === false) {
+        isRepeating = true;
+        repeatButton.querySelector('.bi').classList.remove('bi-repeat');
+        repeatButton.querySelector('.bi').classList.add('bi-repeat-1');
+        repeatButton.classList.add('button-active');
+    } else {
+        isRepeating = false;
+        repeatButton.querySelector('.bi').classList.remove('bi-repeat-1');
+        repeatButton.querySelector('.bi').classList.add('bi-repeat');
+        repeatButton.classList.remove('button-active');
+    }
+}
+
+function nextOrRepeat() {
+    if (isRepeating === false) {
+        nextSong();
+    }
+    else {
+        playsong();
+    }
+}
+
+function timeProgress(originalNumber) {
+    let hours = Math.floor(originalNumber / 3600);
+    let min = Math.floor((originalNumber - hours * 3600) / 60);
+    let secs = Math.floor(originalNumber - hours * 3600 - min *60);
+
+    return`${hours.toString().padStart(2, '0')}:${min.toString().padStart(2, '0')}:${secs.toString().padStart(2, '0')}`;
+}
+
+function updateTotalTime() {
+    totalTime.innerText = timeProgress(song.duration);
+}
+
+function likeButtonManipulate() {
+    if (sortedPlaylist[index].liked === true) {
+        likeButton.querySelector('.bi').classList.remove('bi-heart');
+        likeButton.querySelector('.bi').classList.add('bi-heart-fill');
+        likeButton.classList.add('button-active');
+    }
+    else {
+        likeButton.querySelector('.bi').classList.remove('bi-heart-fill');
+        likeButton.querySelector('.bi').classList.add('bi-heart');
+        likeButton.classList.remove('button-active');
+    }
+}
+
+function likeButtonClicked() {
+    if (sortedPlaylist[index].liked === false) {
+        sortedPlaylist[index].liked = true;
+    } else {
+        sortedPlaylist[index].liked = false;
+    }
+
+    likeButtonManipulate();
+    localStorage.setItem('Playlist', JSON.stringify(originalPlaylist));
 }
 
 
-initializeSong()
-
-play.addEventListener('click', playPauseDecider);           //click = evento de clicar
-previous.addEventListener('click', previousSong);
-next.addEventListener('click', nextSong);
-song.addEventListener('timeupdate', updateProgressBar);     //timeupdate = evento de atualização do tempo da musica
-progressContainer.addEventListener('click', jumpTo);
+// Executa as funções de acordo com os seus requisitos:
+initializeSong();                                               //Evento que puxa os atributos da musica e apresenta na interface do usuario
+play.addEventListener('click', playPauseDecider);               //Evento de clicar (Pause)
+previous.addEventListener('click', previousSong);               //Evento de clicar (Anterior)
+next.addEventListener('click', nextSong);                       //Evento de clicar (Próximo)
+shuffleButton.addEventListener('click', shuffleButtonClicked);  //Evento de clicar (embaralhar)
+repeatButton.addEventListener('click', repeatButtonClicked);    //Evento de clicar (Repetir)
+likeButton.addEventListener('click', likeButtonClicked);        //Evento de clicar (Favoritar)
+progressContainer.addEventListener('click', jumpTo);            //Evento de clicar (Pular tempo da musica)
+song.addEventListener('timeupdate', updateProgress);            //Evento de atualização do tempo atual da musica
+song.addEventListener('loadedmetadata', updateTotalTime);       //Evento de atualização do tempo total da musica
+song.addEventListener('ended', nextOrRepeat);                   //Evento de atualização do final da musica
